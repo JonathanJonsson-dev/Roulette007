47dbb9da637f9b758077967652b91daa44f5636f
Bernster01 <casperberneklint@gmail.com>
2021-09-07 11:26:02 +0200

    Added BaseViewModel and Fody with notifychanged

---
+++ 007/ViewModels/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _007.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel
+    {
+    }
+}
