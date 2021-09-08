09fb7adb161c814f150ed2657a508f4a69ad1583
Bernster01 <casperberneklint@gmail.com>
2021-09-08 10:13:29 +0200

    Added class bet and enum BetType

---
+++ 007/Models/Bet.cs
@@ -0,0 +1,14 @@
+﻿using _007.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _007.Models
+{
+    public class Bet
+    {
+        public BetType Type { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
