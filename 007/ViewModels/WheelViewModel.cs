284332544d5f276cf05146b6c788dec42eb32e8a
Colin_Lehnjoh <lehnjoh@yahoo.com>
2021-09-08 16:36:00 +0200

    WheelModel and WheelViewModel classes

---
+++ 007/ViewModels/WheelViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _007.ViewModels
+{
+    public class WheelViewModel
+    {
+        int winningNumber = -1;
+
+
+    }
+}
