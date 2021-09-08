d43dfd8cb7ca9164673831e369721b12de00e35e
Colin_Lehnjoh <lehnjoh@yahoo.com>
2021-09-08 20:36:11 +0200

    Method that determines winning number after wheel spin

--- 007/ViewModels/WheelViewModel.cs
+++ 007/ViewModels/WheelViewModel.cs
@@ -1,13 +1,84 @@
-﻿using System;
+﻿using _007.Models;
+using _007.Views;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace _007.ViewModels
 {
     public class WheelViewModel
     {
-        int winningNumber = -1;
+        //variables
+        private readonly Wheel wheel = new Wheel();
 
+        //int wheelAngel;
 
+        //wheel numbers collection
+
+        public ObservableCollection<BoardPiece> Wheel { get; set; } = new ObservableCollection<BoardPiece>();
+
+        public WheelViewModel()
+        {
+            FillWheel();
+        }
+
+        /// <summary>
+        /// Method fill wheel with numbers
+        /// </summary>
+        private void FillWheel()
+        {
+             for(int i=0; i>wheel.wheelNumbers.Length; i++)
+            {
+                if (i == 0)
+                {
+                    BoardPiece boardPiece = new BoardPiece
+                    {
+                        BoardPieceColor = Brushes.Green,
+                        BoardPieceNumber = wheel.wheelNumbers[i],
+                    };
+
+                    Wheel.Add(boardPiece);
+                }
+                else if(i % 2 != 0)
+                {
+                    BoardPiece boardPiece = new BoardPiece
+                    {
+                        BoardPieceColor = Brushes.Red,
+                        BoardPieceNumber = wheel.wheelNumbers[i],
+                    };
+
+                    Wheel.Add(boardPiece);
+                }
+                else
+                {
+                    BoardPiece boardPiece = new BoardPiece
+                    {
+                        BoardPieceColor = Brushes.Black,
+                        BoardPieceNumber = wheel.wheelNumbers[i],
+                    };
+
+                    Wheel.Add(boardPiece);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Method determines winning number after wheel spin
+        /// </summary>
+        /// <param name="angel"></param>
+        /// <returns></returns>
+        private int DetermineWinningNumber(int angel)
+        {
+            //Each number represents a sector of a circle. Determine angle of each sector out of 37
+            decimal sectorAngle = 360 / 37;
+
+            int determinant = (int)Math.Truncate(angel / sectorAngle); //gives position of winning number in array of wheel numbers
+
+            return wheel.wheelNumbers[determinant];
+        }
     }
 }
