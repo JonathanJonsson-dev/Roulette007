c1b1a04986acc3b21bd581bea4ea15e94e012b7f
Bernster01 <casperberneklint@gmail.com>
2021-09-13 14:06:35 +0200

    Added button to close inputbox for betting

---
+++ 007/Commands/CloseBetCommand.cs
@@ -0,0 +1,30 @@
+﻿using _007.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace _007.Commands
+{
+    class CloseBetCommand : ICommand
+    {
+        private readonly GameViewModel gameViewModel;
+        public CloseBetCommand(GameViewModel gameViewModel)
+        {
+            this.gameViewModel = gameViewModel;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+
+            gameViewModel.BoardViewModel.CurrentBet.Clear();
+
+        }
+    }
+}
