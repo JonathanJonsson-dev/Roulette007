57ea6901f957ebbd7c89d3a077baf82b43540354
Bernster01 <casperberneklint@gmail.com>
2021-09-17 11:56:56 +0200

    Added Marker usercontrol

---
+++ 007/Views/Marker.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _007.Views
+{
+    /// <summary>
+    /// Interaction logic for Marker.xaml
+    /// </summary>
+    public partial class Marker : UserControl
+    {
+        public Marker()
+        {
+            InitializeComponent();
+        }
+
+
+        public SolidColorBrush Color
+        {
+            get { return (SolidColorBrush)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Color.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(SolidColorBrush), typeof(Marker), new PropertyMetadata(Brushes.Black));
+
+
+    }
+}
