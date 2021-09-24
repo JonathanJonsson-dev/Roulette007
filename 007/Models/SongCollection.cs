b437f9bce9fcd6eda41112b173726411e1df97a7
Jonathan Jonsson <jonsson.jonathan@hotmail.com>
2021-09-24 14:56:14 +0200

    Added 007 Songs

--- 007/Models/SongCollection.cs
+++ 007/Models/SongCollection.cs
@@ -10,7 +10,9 @@ namespace _007.Models
     {
         
         public List<Song> songs = new List<Song>
-            {   new Song(){ Filepath = new Uri(@"Resources\Songs\MemoirOfSummer.mp3", UriKind.Relative)},
+            {   new Song(){ Filepath = new Uri(@"Resources\Songs\007JamesBondTheme.mp3", UriKind.Relative)},
+                new Song(){ Filepath = new Uri(@"Resources\Songs\TheJohnBarryOrchestraJamesBondTheme.mp3", UriKind.Relative)},
+                new Song(){ Filepath = new Uri(@"Resources\Songs\MemoirOfSummer.mp3", UriKind.Relative)},
                 new Song(){ Filepath = new Uri(@"Resources\Songs\GlideWithMe.mp3", UriKind.Relative)},
                 new Song(){ Filepath = new Uri(@"Resources\Songs\PathwayToHaven.mp3", UriKind.Relative)},
                 new Song(){ Filepath = new Uri(@"Resources\Songs\ByYourSide.mp3", UriKind.Relative)}
